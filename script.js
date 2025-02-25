9f30509b7885e4b44878b0c64dbcc78217f391b7
alexsykes8 <65182304+alexsykes8@users.noreply.github.com>
2025-02-25 15:16:51 +0000

    allanimuploads

--- script.js
+++ script.js
@@ -11,5 +11,37 @@ document.addEventListener('DOMContentLoaded', () => {
       video.currentTime = 0;
     });
   });
+
+  const videosvtube = document.querySelectorAll('.vtube-container video');
+
+  videosvtube.forEach(video => {
+    video.addEventListener('mouseenter', () => {
+      video.play();
+    });
+
+    video.addEventListener('mouseleave', () => {
+      video.pause();
+      video.currentTime = 0;
+    });
+  });
 });
+
+function toggleBanner(element) {
+  // Find the closest expandable-banner to the clicked arrow
+  const banner = element.closest('.expandable-banner');
+
+  // Toggle the "open" class on the correct banner
+  banner.classList.toggle('open');
+
+  // Change the text inside the arrow
+  const arrowText = banner.querySelector('.arrow p');
+  if (banner.classList.contains('open')) {
+    arrowText.textContent = "[Collapse]";
+  } else {
+    arrowText.textContent = "[Expand]";
+  }
+}
+
+
+
 
